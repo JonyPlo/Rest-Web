c2c64b251e9af14b59a5e5eed7e402065ff7f269
JonyPlo <joonyyplo@gmail.com>
2024-06-09 09:37:32 -0300

    change the server class to receive env vars

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
+import { envs } from './config/envs'
 import { Server } from './presentation/server'
-
 ;(() => {
   main()
 })()
 
 function main() {
-  const server = new Server()
+  const server = new Server({ port: envs.PORT, public_path: envs.PUBLIC_PATH })
 
   server.start()
 }

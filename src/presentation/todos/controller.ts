42aeafd10f2d55e869fd71980a12410c0c33928a
JonyPlo <joonyyplo@gmail.com>
2024-06-16 22:32:03 -0300

    add: implement use cases in todo controllers to manage todos

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express'
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos'
-import { TodoRepository } from '../../domain'
+import {
+  GetTodos,
+  TodoRepository,
+  UpdateTodo,
+  GetTodo,
+  CreateTodo,
+  DeleteTodo,
+} from '../../domain'
 
 // La aplicacion de esta arquitectura se llama Domain Driven Design (DDD) y Repository Pattern (RP), quiere decir que todo esta hecho basado en repositorios, datasources, separacion de responsabilidad unica, y todo lo que esta implementado
 //* Aqui SI estamos aplicando casos de uso (use cases)
@@ -9,32 +16,34 @@ export class TodosController {
   // Inyectamos en el constrolador de los todos, la implementacion del repositorio de los todos para poder dar de alta todos, editarlos, etc
   constructor(private readonly todoRepository: TodoRepository) {}
 
-  public getTodos = async (req: Request, res: Response) => {
-    const todos = await this.todoRepository.getAll()
-    return res.json(todos)
+  public getTodos = (req: Request, res: Response) => {
+    new GetTodos(this.todoRepository)
+      .execute()
+      .then((todos) => res.json(todos))
+      .catch((error) => res.status(400).json({ error }))
   }
 
-  public getTodoById = async (req: Request, res: Response) => {
+  public getTodoById = (req: Request, res: Response) => {
     const id = +req.params.id
 
-    try {
-      const todo = await this.todoRepository.findById(id)
-      res.json(todo)
-    } catch (error) {
-      res.status(400).json({ error })
-    }
+    new GetTodo(this.todoRepository)
+      .execute(id)
+      .then((todo) => res.json(todo))
+      .catch((error) => res.status(400).json({ error }))
   }
 
-  public createTodo = async (req: Request, res: Response) => {
+  public createTodo = (req: Request, res: Response) => {
     // En este caso la clase CreateTodoDto funciona como un adapter, es decir, recibe el objeto de la query, valida los datos y adapta el objeto a uno valido para poder almacenarlo en la base de datos
     const [errorMessage, createTodoDto] = CreateTodoDto.create(req.body)
     if (errorMessage) return res.status(400).json({ message: errorMessage })
 
-    const todo = await this.todoRepository.create(createTodoDto!)
-    res.json(todo)
+    new CreateTodo(this.todoRepository)
+      .execute(createTodoDto!)
+      .then((todo) => res.json(todo))
+      .catch((error) => res.status(400).json({ error }))
   }
 
-  public updateTodo = async (req: Request, res: Response) => {
+  public updateTodo = (req: Request, res: Response) => {
     const id = +req.params.id
     const [errorMessage, updateTodoDto] = UpdateTodoDto.create({
       ...req.body,
@@ -42,13 +51,19 @@ export class TodosController {
     })
 
     if (errorMessage) return res.status(400).json({ errorMessage })
-    const updatedTodo = await this.todoRepository.updateById(updateTodoDto!)
-    res.json(updatedTodo)
+
+    new UpdateTodo(this.todoRepository)
+      .execute(updateTodoDto!)
+      .then((todo) => res.json(todo))
+      .catch((error) => res.status(400).json({ error }))
   }
 
-  public deleteTodo = async (req: Request, res: Response) => {
+  public deleteTodo = (req: Request, res: Response) => {
     const id = +req.params.id
-    const deletedTodo = await this.todoRepository.deleteById(id)
-    res.json(deletedTodo)
+
+    new DeleteTodo(this.todoRepository)
+      .execute(id)
+      .then((todo) => res.json(todo))
+      .catch((error) => res.status(400).json({ error }))
   }
 }
